53464ba704c6251e6510806a612450411ce62b4f
Gauri Laxman <gaurilaxman>
2018-02-10 05:19:53 -0500

    First commit

---
+++ app/src/main/java/com/example/gauri/barcodescanner/ScanBarcodeActivity.java
@@ -0,0 +1,84 @@
+package com.example.gauri.barcodescanner;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+/**
+ * Created by gauri on 2/10/18.
+ */
+
+public class ScanBarcodeActivity extends Activity{
+    SurfaceView cameraPreview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_barcode);
+
+        cameraPreview = (SurfaceView)findViewById(R.id.camera_preview);
+        createCameraSource();
+    }
+
+    private void createCameraSource() {
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
+        final CameraSource cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setAutoFocusEnabled(true)
+                .setRequestedPreviewSize(1600,1024)
+                .build();
+
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes=detections.getDetectedItems();
+                if(barcodes.size()>0) {
+                    Intent intent=new Intent();
+                    intent.putExtra("barcode",barcodes.valueAt(0)); //get latest barcode from the array
+                    setResult(CommonStatusCodes.SUCCESS,intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
